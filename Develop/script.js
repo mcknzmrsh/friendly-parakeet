c6d0546d8ca9ad0d32fe2b8a4771c78a17afa088
mcknzmrsh <marsh.mackenzie@gmail.com>
2022-11-09 13:32:57 -0500

    added code to js

--- Develop/script.js
+++ Develop/script.js
@@ -6,12 +6,32 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
   var password = generatePassword();
+}
+  
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var password=document.getElementById("password");
 
-}
+  function genPassword() {
+     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     var passwordLength = 12;
+     var password = "";
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+   }
+         document.getElementById("password").value = password;
+  }
+ 
+ function copyPassword() {
+   var copyText = document.getElementById("password");
+   copyText.select();
+   document.execCommand("copy");  
+ }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
