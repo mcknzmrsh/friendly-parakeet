99e50bd5618665006a6b205b7bea9e35f4dd857a
mcknzmrsh <marsh.mackenzie@gmail.com>
2022-11-09 13:57:31 -0500

    added code to css and js

--- Develop/script.js
+++ Develop/script.js
@@ -16,7 +16,7 @@ function writePassword() {
 
   function genPassword() {
      var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-     var passwordLength = 12;
+     var passwordLength = 8;
      var password = "";
   for (var i = 0; i <= passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
